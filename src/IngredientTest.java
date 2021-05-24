778d84600d2fd743798abaef78fadf78e93933d7
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-24 18:34:49 +0200

    change spring print

--- src/IngredientTest.java
+++ src/IngredientTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IngredientTest {
     @Test
     public void print(){
-        Ingredient ingredient =new Ingredient("peper");
-        assertEquals("peper",ingredient.print());
+        Ingredient ingredient =new Ingredient("peper en zout naar smaak");
+        assertEquals("peper en zout naar smaak",ingredient.print());
     }
 }
