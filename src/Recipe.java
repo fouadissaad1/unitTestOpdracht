254eec33b25ff0fc31f7b575494ad3b9ca70ff78
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-26 19:24:12 +0200

    add funtion addIngredient and print

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,4 +1,11 @@
 public class Recipe {
     String dishName;
+    public void addIngredient(Ingredient ingredient){
+
+    }
+    public String print(int amountOfPortions){
+
+        return "";
+    }
 
 }
