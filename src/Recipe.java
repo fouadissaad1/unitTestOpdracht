cf7032a5fa5b83abc689d452b523b0b5974ac093
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-26 19:42:46 +0200

    add in class object

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,9 +1,11 @@
 public class Recipe {
     String dishName;
+    private int aantalPersoon;
 
 
     public Recipe(String dishName, int aantalPersoon) {
         this.dishName = dishName;
+        this.aantalPersoon = aantalPersoon;
     }
 
     public void addIngredient(Ingredient ingredient) {
