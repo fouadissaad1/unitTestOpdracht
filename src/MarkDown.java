a4c2106ff7cdc2bdcd734e0ccb18229b80c13f09
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-05-26 22:48:05 +0200

    add class MarkDown and test

---
+++ src/MarkDown.java
@@ -0,0 +1,13 @@
+public class MarkDown {
+
+
+    private static String [] String;
+
+    public MarkDown() {
+    }
+
+    public static String[] format(java.lang.String[] s)
+    {
+        return String;
+    }
+}
---
+++ src/MarkDownTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarkDownTest {
+    @Test
+    public void printArry(){
+        MarkDown markDown=new MarkDown();
+
+
+    }
+
+
+}
