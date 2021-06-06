85cade5afbefccbe69cdaad2c2294d09e434224a
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 16:55:32 +0200

    add test cases if 0 retour always peper en zout

--- src/IngredientTest.java
+++ src/IngredientTest.java
@@ -8,4 +8,10 @@ public class IngredientTest {
         Ingredient ingredient=new Ingredient("perper en zout naar smaak");
         assertEquals("peper en zout naar smaak",ingredient.print(1));
     }
+
+    @Test
+    public void oneParameterWithPrint0What(){
+        Ingredient ingredient=new Ingredient("perper en zout naar smaak");
+        assertEquals("peper en zout naar smaak",ingredient.print(0));
+    }
 }
