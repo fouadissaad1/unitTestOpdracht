c5b62aa06054a0256fb9fa937198a6124fdf4a40
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 18:19:22 +0200

    add class with parameter.

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,40 +1,8 @@
-import java.util.ArrayList;
-
 public class Recipe {
-    String dishName;
-    private int nrOfPortions;
-    ArrayList<Ingredient> ingredients = new ArrayList();
-
-
-
-    public Recipe(String dishName, int nrOfPortions) {
-        this.dishName = dishName;
-        this.nrOfPortions = nrOfPortions;
-    }
-
     public Recipe(String dishName) {
-        this.dishName = dishName;
     }
-
-    public void addIngredient(Ingredient ingredient) {
-         ingredients.add(ingredient);
-
-    }
-
 
     public String print(int amountOfPortions) {
-
-        if (amountOfPortions==0) return null;
-        if (amountOfPortions==1) return this.dishName +" voor "+this.nrOfPortions+" persoon";
-        for (Ingredient  ingredient:ingredients) {
-                 ingredient.print(1);
-                 return ingredient.amount+","+ingredient.singular+","+ingredient.what;
-        }
-        if (amountOfPortions>1) return this.dishName+" voor "+this.nrOfPortions+" personnen";
-        return dishName;
+        return "";
     }
-
-
 }
-
-
