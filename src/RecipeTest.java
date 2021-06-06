5882006f4f62f9b64ff4afcaf078b7f7d305ef85
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 18:16:36 +0200

    add new class Recipe

--- src/RecipeTest.java
+++ src/RecipeTest.java
@@ -1,39 +1,12 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecipeTest {
     @Test
-    public void tileReceptReturn(){
-        Recipe recipe=new Recipe("appeltaart");
-        assertEquals("appeltaart voor 0 persoon",recipe.print(1));
-    }
 
-    @Test
-    public  void amountOfPortionsNullIs(){
-        Recipe recipe=new Recipe(null);
-        assertEquals(null,recipe.print(0));
-    }
-
-    @Test
-    public void addAantalPersoon(){
-        Recipe recipe=new Recipe("appeltart",10);
-        assertEquals("appeltart voor 10 personnen",recipe.print(10));
-    }
-
-    @Test
-    public void correctAntalSinglePersonne(){
-        Recipe recipe=new Recipe("appeltart",1);
-        assertEquals("appeltart voor 1 persoon",recipe.print(1));
-    }
-
-
-    @Test
-    public void correct(){
-        Recipe recipe=new Recipe("appeltart",1);
-        Ingredient ingredient=new Ingredient(10,"kopje","kopjes","bloem");
-        recipe.addIngredient(ingredient);
-        assertEquals("appeltart voor 1 persoon",recipe.print(1));
+    public void printRecipe(){
+        Recipe recipe = new Recipe("appeltaart");
+        assertEquals("appeltaart voor 10 personen", recipe.print(10));
 
     }
 
