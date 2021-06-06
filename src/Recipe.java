fad7229c05c77f87c774dd1fbb6321ac271c7107
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 19:55:30 +0200

    add in class recipe constructor with two parameter.

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,8 +1,14 @@
 public class Recipe {
     private String dishName;
+    private Ingredient ingredient;
 
     public Recipe(String dishName) {
         this.dishName = dishName;
+    }
+
+    public Recipe(String dishName, Ingredient ingredient) {
+        this.dishName = dishName;
+        this.ingredient = ingredient;
     }
 
     public String print(int amountOfPortions) {
