35a66902542886d15513e83283552f4dbcc48e1f
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 17:15:45 +0200

    hier i have error with actual next step resolved issue

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,15 +1,24 @@
 public class Ingredient {
     private String what;
+    private int amount;
+    private String singular;
+    private String plural;
 
     public Ingredient(String what) {
         this.what = what;
     }
 
     public Ingredient(int amount, String singular, String plural) {
+        this.amount = amount;
+        this.singular = singular;
+        this.plural = plural;
     }
 
     public String print(int  nrOfPortions) {
         String text="peper en zout naar smaak";
-        return text;
+
+        if (what==null && amount==1) return nrOfPortions+" "+this.singular;
+
+      return text;
     }
 }
