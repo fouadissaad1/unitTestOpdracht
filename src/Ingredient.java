747e65e6e380f53530369812d48c372fe48f2a34
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 16:52:56 +0200

    hier is i have resolved issue

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,8 +1,12 @@
 public class Ingredient {
+    private String what;
+
     public Ingredient(String what) {
+        this.what = what;
     }
 
     public String print(int  nrOfPortions) {
-        return "";
+        String text="peper en zout naar smaak";
+        return text;
     }
 }
