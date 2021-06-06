f08dc0e43b7783d85c6e3eaf5a170acd0147d02c
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-06 16:46:06 +0200

    OneParamenterWhat Test

--- src/IngredientTest.java
+++ src/IngredientTest.java
@@ -1,54 +1,9 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class IngredientTest {
     @Test
-    public void printWhat(){
-        Ingredient ingredient =new Ingredient("peper en zout");
+    public void oneParameterWhat(){
+        Ingredient ingredient=new Ingredient("perper en zout naar smaak");
         assertEquals("peper en zout naar smaak",ingredient.print(1));
     }
-
-    @Test
-    public  void PrintWhatNullAnNrOfPortionNull(){
-        Ingredient ingredient =new Ingredient(null);
-        assertEquals(null,ingredient.print(0));
-    }
-
-    @Test
-    public void printThreParamenter(){
-        Ingredient ingredient =new Ingredient(1,"appel","appels");
-        assertEquals("1,appel",ingredient.print(1));
-    }
-    @Test
-    public void printOneAmoutvoidMet3Parameter(){
-        Ingredient ingredient =new Ingredient(1,"appel","appels");
-        assertEquals("1,appel",ingredient.print(1));
-    }
-
-    @Test
-    public void printMeervoidMet3ParameterMoreDen2NrOfPortion(){
-        Ingredient ingredient =new Ingredient(2,"knoflookteen","knoflooktenen");
-        assertEquals("2,knoflooktenen",ingredient.print(2));
-    }
-
-    @Test
-    public void printMeervoidMet4Parameter1NrOfPortion(){
-        Ingredient ingredient =new Ingredient(1,"eetlepel","eetelps","suiker");
-        assertEquals("1,eetlepel,suiker",ingredient.print(1));
-   }
-
-
-    @Test
-    public void printMeervoidMet4ParameterMorethat4NrOfPortion(){
-        Ingredient ingredient =new Ingredient(4,"eetlepel","eetelps","suiker");
-        assertEquals("4,eetelps,suiker",ingredient.print(4));
-    }
-
-    @Test
-    public void printNrOfPortion5And2Amout(){
-        Ingredient ingredient =new Ingredient(2,"kopje","kopjes","bloem");
-        assertEquals("5,kopjes,bloem",ingredient.print(5));
-    }
-
-
 }
