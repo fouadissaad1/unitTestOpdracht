d4b8fe1589d1f324cb8a6fc91fa7927139aacdd4
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-08 22:37:55 +0200

    add test Mardown

--- src/MarkDownTest.java
+++ src/MarkDownTest.java
@@ -1,41 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class MarkDownTest{
-    @Test
-    public void printSplistArray(){
-        MarkDown markDown=new MarkDown();
-        assertArrayEquals(new String[]{"blablab,*b"},markDown.format( "blablab *b"));
-
-    }
+public class MarkDownTest {
 
     @Test
-    public void printArrstartChangeFormatbold(){
-        MarkDown markDown=new MarkDown();
-        assertArrayEquals(new String[]{"<b>,blabla,bla"},markDown.format( "*blabla bla"));
-
+    public void printformat(){
+        MarkDown markDown= MarkDown();
+        String[] arr1 = markdown.format("blabla bla");
+        String[] arr2 = markdown.format("blabla bla");
+        assertArrayEquals(arr1,arr2);
     }
-
-    @Test
-    public void printArrEndChangeFormatbold(){
-        MarkDown markDown=new MarkDown();
-        assertArrayEquals(new String[]{"blabla,bla,</b>"},markDown.format( "blabla bla*"));
-
-    }
-
-
-    @Test
-    public void printArr_Null(){
-        MarkDown markDown=new MarkDown();
-        assertArrayEquals(null,markDown.format( null));
-
-    }
-
-
-
-
 }
