aae2b4ab9b319dc6ef9ea70d2e7dc0fca4176e27
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-06-08 22:40:39 +0200

    add test Mardown

--- src/MarkDown.java
+++ src/MarkDown.java
@@ -1,32 +1,5 @@
-import org.junit.platform.commons.util.StringUtils;
-
 public class MarkDown {
-
-
-
-
-    public MarkDown() {
+    public String[] format(String s) {
+        return s;
     }
-    public static String[] format(String s)
-    {
-
-
-
-        if (s==null) return null;
-
-        String[] words=new String[]{};
-
-        words=s.split(" * ");
-
-        String newName=String.join(",",words);
-
-        if(newName.endsWith("*")) return new String[]{newName.replace("*",",</b>")};
-        if(newName.startsWith("*")) return new String[]{newName.replace("*","<b>,")};
-        if (newName.endsWith("* ")) return new String[]{newName.replace("*","</b>,")};
-
-
-        return new String[]{newName};
-    }
-
-
 }
--- src/MarkDownTest.java
+++ src/MarkDownTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MarkDownTest {
 
     @Test
     public void printformat(){
         MarkDown markDown= MarkDown();
-        String[] arr1 = markdown.format("blabla bla");
-        String[] arr2 = markdown.format("blabla bla");
+        String[] arr1 = markDown.format("blabla bla");
+        String[] arr2 = markDown.format("blabla bla");
         assertArrayEquals(arr1,arr2);
     }
 }
